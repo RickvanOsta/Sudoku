988d1e9e2618244d6ae98d0d5be5923deb7ea165
DnJealt <jlm.vantartwijk@student.avans.nl>
2014-12-02 14:30:46 +0100

    Class Library added\
    What it says on the tin

--- Sudoku/Sudoku/MainWindow.xaml.cs
+++ Sudoku/Sudoku/MainWindow.xaml.cs
@@ -23,5 +23,5 @@ namespace Sudoku
         {
             InitializeComponent();
         }
-    }
+    } 
 }
---
+++ Sudoku/SudokuLibrary/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sudoku;
+
+namespace SudokuLibrary
+{
+    public class Class1
+    {
+
+
+        private IGame game;
+
+        public Class1()
+        {
+            game = new Sudoku.Game();
+        }
+
+        public bool CreateGameAndWrite()
+        {
+            int i;
+            game.create();
+            game.write(out i);
+            return i == 1;
+        }
+
+
+    }
+}
