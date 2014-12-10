0c5014b2218c6578ec936b5b6b018176ad0518e9
DnJealt <jlm.vantartwijk@student.avans.nl>
2014-12-10 11:29:34 +0100

    sdc
    dcv

--- Sudoku/Sudoku/ViewModel/MainViewModel.cs
+++ Sudoku/Sudoku/ViewModel/MainViewModel.cs
@@ -21,14 +21,7 @@ namespace Sudoku.ViewModel
         /// </summary>
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+           
         }
     }
 }
